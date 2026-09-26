5289d24a4dee7259c6e19b3bbb8ca4037cf7f242
Minh Thịnh <thinhvm.contact@gmail.com>
2026-09-26 09:40:41 +0700

    up

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ const albumImages = [
     'image/DOJ_6851_1.webp',
     'image/DOJ_7032_1.webp',
     'image/DOJ_6149_1.webp',
-    'image/DOJ_6509_1.webp',
+    'image/DOJ_7271_1.webp',
     'image/DOJ_7063_1.webp',
-    'image/DOJ_7215_1.jpg'
+    
 ];
 
 let currentimageIndex = 0;
